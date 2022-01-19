ee5a89d86e02db1f312a5fc7acfdfa000e0aa324
Hannah Boesger <hb054617@ohio.edu>
2022-01-19 17:13:44 -0500

    Hello Gitgit add git-lab-program.cc! added

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Hannah Boesger>
+ *        Date: <01/19/2022>
+ * Description: CS2400 lab 2 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!!!" << endl;
+    return 0;
+}// main
